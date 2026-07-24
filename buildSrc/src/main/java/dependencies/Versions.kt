aad9ea9ff6d364df99ed11ccb38ded6ec2381656
ulymaglana <ulysses@maglana.com>
2026-07-24 18:36:49 +0800

    Update Versions.kt

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -4,9 +4,8 @@ object Versions {
     const val android_compile_sdk = 35
     const val android_min_sdk = 21
     const val android_target_sdk = 35
-
     const val androidx_fragment = "1.8.1"
-    const val dagger = "2.51.1"
+    const val dagger = "2.56.1"
     const val espresso = "3.6.1"
     const val glide = "4.16.0"
     const val okhttp3 = "4.12.0"
